e322461c1a1b7298c26c8d5f929d86449f1d866c
Francesco Sorrentino <francesco.sorrentino@studenti.unitn.it>
2024-04-24 16:57:30 +0200

    Added npm modules mongoose, cors and nodenv and also created basic server

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+// Configurazione middleware di parsing
+app.use(express.json());
+
+// Lettura a schermo delle richieste
+app.use((req,res,next) => {
+    console.log(req.method + ' ' + req.url)
+    next()
+})
+
+// Richieste CORS
+app.use(cors())
+
+// Handler errore 404 NOT FOUND di default
+app.use((req, res) => {
+    res.status(404);
+    res.json({ error: 'NOT FOUND' });
+});
+
+module.exports = app;

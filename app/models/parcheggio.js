0e83070e0fe0259c218756ec09cf6ef5aabe0797
Francesco Sorrentino <francesco.sorrentino@studenti.unitn.it>
2024-04-30 16:04:38 +0200

    integer values fix added

--- app/models/parcheggio.js
+++ app/models/parcheggio.js
@@ -6,15 +6,15 @@ const parcheggioSchema = mongoose.Schema({
     _id : { type: mongoose.Schema.Types.ObjectId, required: true},
     nome : { type: String, required: true},
     posizione : { type: pointSchema, required: true },
-    numPosti : { type: Number, required: true, min: 1 },
+    numPosti : { type: Number, required: true, min: 1, get: v => Math.round(v),set: v => Math.round(v) },
     isCoperto : { type: Boolean, required: true},
     statoParcheggio : { type: String, required: true, enum: ["Disponibile", "Non disponibile"]},
-    numPostiDisabili : { type: Number, required: true, min: 1},
-    numPostiGravidanza : { type: Number, required: true, min: 1},
-    numPostiAuto : { type: Number, required: true, min: 1},
-    numPostiMoto : { type: Number, required: true, min: 1},
-    numPostiFurgone : { type: Number, required: true, min: 1},
-    numPostiBus : { type: Number, required: true, min: 1}
+    numPostiDisabili : { type: Number, required: true, min: 1, get: v => Math.round(v),set: v => Math.round(v)},
+    numPostiGravidanza : { type: Number, required: true, min: 1, get: v => Math.round(v),set: v => Math.round(v)},
+    numPostiAuto : { type: Number, required: true, min: 1, get: v => Math.round(v),set: v => Math.round(v)},
+    numPostiMoto : { type: Number, required: true, min: 1, get: v => Math.round(v),set: v => Math.round(v)},
+    numPostiFurgone : { type: Number, required: true, min: 1, get: v => Math.round(v),set: v => Math.round(v)},
+    numPostiBus : { type: Number, required: true, min: 1, get: v => Math.round(v),set: v => Math.round(v)}
 }, { discriminatorKey : '_type' });
 
 module.exports = mongoose.model('Parcheggio', parcheggioSchema);
--- app/models/postiOccupati.js
+++ app/models/postiOccupati.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 // Schema che definisce i posti occupati
 const postiOccupatiSchema = new mongoose.Schema({
-    postiOcc : { type: Number, required: true, min: 0 },
-    postiOccDisabili : { type: Number, required: true, min: 0},
-    postiOccGravidanza : { type: Number, required: true, min: 0},
-    postiOccAuto : { type: Number, required: true, min: 0},
-    postiOccMoto : { type: Number, required: true, min: 0},
-    postiOccFurgone : { type: Number, required: true, min: 0},
-    postiOccBus : { type: Number, required: true, min: 0}
+    postiOcc : { type: Number, required: true, min: 0, get: v => Math.round(v),set: v => Math.round(v) },
+    postiOccDisabili : { type: Number, required: true, min: 0, get: v => Math.round(v),set: v => Math.round(v)},
+    postiOccGravidanza : { type: Number, required: true, min: 0, get: v => Math.round(v),set: v => Math.round(v)},
+    postiOccAuto : { type: Number, required: true, min: 0, get: v => Math.round(v),set: v => Math.round(v)},
+    postiOccMoto : { type: Number, required: true, min: 0, get: v => Math.round(v),set: v => Math.round(v)},
+    postiOccFurgone : { type: Number, required: true, min: 0, get: v => Math.round(v),set: v => Math.round(v)},
+    postiOccBus : { type: Number, required: true, min: 0, get: v => Math.round(v),set: v => Math.round(v)}
   });
 
 module.export = postiOccupatiSchema;

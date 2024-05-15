6f283780027d165a92ca6c375cbb2016da0ea6d3
Alessandro Del Zenero <alessandro.delzenero@studenti.unitn.it>
2024-05-15 10:55:28 +0200

    Fix errori token, configurazione deployement

--- app/middleware/checkAuth.js
+++ app/middleware/checkAuth.js
@@ -2,29 +2,29 @@ const jwt = require('jsonwebtoken');
 
 // Autorizzazione
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
+    if (!req.headers.authorization) {
         return res.status(401).json({
             error: 'No token provided'
         })
     }
+    const token = req.headers.authorization.split(" ")[1];
     jwt.verify(token, process.env.JWT_KEY, (err, decodedToken) => {
         if (err) {
             switch (err.name) {
                 case 'TokenExpiredError': {
-                    return res.status(401).json({
+                    return res.status(403).json({
                         error: err.message // 'jwt expired'
                     });
                     break;
                 }
                 case 'JsonWebTokenError': {
-                    return res.status(401).json({
+                    return res.status(403).json({
                         error: err.message
                     });
                 }
                 case 'NotBeforeError': {
-                    return res.status(401).json({
-                        error: err.message
+                    return res.status(403).json({
+                        error: err.message // 'jwt not active'
                     })
                 }
                 default: {
